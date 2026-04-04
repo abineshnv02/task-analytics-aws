52fd180d96185f9af52e730c4d0ec88da1eaa090
abineshnv02 <abineshnv02@gmail.com>
2026-04-04 17:05:16 +0530

    added a string

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+# See here https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z$i6d$ozv(hk_-#o8x503st(+2_$hz4sl!dd(mjdwny*blact5'
