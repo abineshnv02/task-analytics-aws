fcaa8430895a3334fa8c33c0563070748738682a
abineshnv02 <abineshnv02@gmail.com>
2026-04-04 11:46:07 +0530

    Initial django setuo with repository name task-analytics

---
+++ analytics/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('H', 'High'),
+        ('M', 'Medium'),
+        ('L', 'Low'),
+    ]
+    title = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default='M')
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
