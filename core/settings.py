43914eebcbd918da2ff6dbdc0c8cf4dbaf1ea655
abineshnv02 <abineshnv02@gmail.com>
2026-04-05 11:13:14 +0530

    updated ip

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z$i6d$ozv(hk_-#o8x503st(+2_$hz4sl!dd(mjdwny*blact5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.60.186.44', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['54.252.162.206', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -114,4 +114,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL
