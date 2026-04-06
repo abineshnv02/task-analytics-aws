677d62c24bfef94be78e20b24ff743a7b89465db
Abinesh N V (EXT-Nokia) <abinesh.n_v.ext@nokia.com>
2026-04-06 21:33:11 +0530

    added a string in views

--- analytics/views.py
+++ analytics/views.py
@@ -4,7 +4,7 @@ def index(request):
     return HttpResponse("""
         <body style="background: linear-gradient(to right, #4facfe, #00f2fe); 
                      font-family: Arial; text-align: center; color: white; padding-top: 100px;">
-            <h1 style="font-size: 50px;">🚀 CI/CD Pipeline Active!</h1>
+            <h1 style="font-size: 50px;">🚀 CI/CD Pipeline Active in your laptop!</h1>
             <p style="font-size: 20px;">
                 Deployed from <b>feature-update-view</b> via GitHub Actions.
             </p>
